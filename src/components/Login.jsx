819bf6f6b0cb5a9cdfa6a423f682de6d1f80d664
Malek Mneimneh <106251533+malek0x1@users.noreply.github.com>
2022-06-23 23:29:00 +0300

    Update Login.jsx

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -19,12 +19,14 @@ const Login = () => {
                 <h1>Login Form</h1>
                 <div className="inpts">
                     <input onChange={(e) => setUserState(e.target.value)} autoComplete='false' autoCorrect='false' placeholder="Type username " /><br />
-                    <input onChange={(e) => setPwdState(e.target.value)} placeholder="Type password" /><br />
+                    <input onChange={(e) => setPwdState(e.target.value)} type="password" placeholder="Type password" /><br />
                     <button onClick={handleLogin} >Click</button>
 
                 </div>
                 {state.isLoggedin && !state.error &&  <span className="success">Login Success !</span>}
                 {state.error &&  <span className="error">Wrong Credentials !</span>}
+                user=malek password=dbpassword
+
             </div>
 
         </div>
